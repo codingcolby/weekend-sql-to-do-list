1361d687258954632a59b004fd54da0582ef4f34
allthethings <learn2code@respectdesfonds.com>
2020-04-05 23:35:37 -0500

    added styling to front end

--- database.sql
+++ database.sql
@@ -1,15 +1,24 @@
 -- In Postgres, first create a database to store your tables and data.
 
--- When creating tables, use an underscore in field names; do not use dashes or spaces. Fields should have some parameters such as date, varchar(250), integer, not null, etc. If needed, copy and paste the examples below, enhance or modify them for your purposes before executing the statement(s).
 
 -- CREATE A NEW TABLE
-CREATE TABLE "sometablename" (
-	"id" serial primary key,
-	"field_1" varchar(75),
-	"field_2" varchar(100),
-	"field_3" smallint not null,
-	"field_4" date
+CREATE TABLE "todolist" (
+	"id" SERIAL PRIMARY KEY,
+	"task_name" VARCHAR(175) NOT NULL,
+	"date_added" DATE NOT NULL,
+	"priority_level" SMALLINT NOT NULL,
+	"resources_needed" VARCHAR(250),
+	"do_delegate" VARCHAR(8),
+	"due_date" DATE,
+	"completed" BOOLEAN
 );
 
+
 -- ADD A RECORD TO THE TABLE (the table must be created first)
-INSERT INTO "sometable"("field_1", "field_2", "field_3", "field_4") VALUES ("samplevalue1", "samplevalue2", 1234, "1/1/1900");
+INSERT INTO "todolist" ("task_name", "date_added", "priority_level", "resources_needed", "do_delegate","due_date", "completed")  VALUES ('Cat maintenance', '4/5/2020', 1, 'cat food, fresh water, litter scoop and plastic bag', 'Do', '4/5/2020', 'true');
+
+INSERT INTO "todolist" ("task_name", "date_added", "priority_level", "resources_needed", "do_delegate","due_date", "completed")  VALUES ('Dishes', '4/5/2020', 1, 'new sponge', 'Do', '4/5/2020', 'true');
+
+INSERT INTO "todolist" ("task_name", "date_added", "priority_level", "resources_needed", "do_delegate","due_date", "completed")  VALUES ('Launder clothes', '4/5/2020', 1, 'sort clothes', 'Do', '4/8/2020', 'false');
+
+INSERT INTO "todolist" ("task_name", "date_added", "priority_level", "resources_needed", "do_delegate","due_date", "completed")  VALUES ('Take out the trash', '4/5/2020', 1, 'add new bag to trash can', 'Delegate', '4/6/2020', 'false');
